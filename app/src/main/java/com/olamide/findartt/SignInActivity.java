6f64c47bb3d3e663ba489cff0b7b54ed075a5fcf
Olamide.Laleye <olamide.laleye@interswitchng.com>
2019-01-25 13:09:18 +0100

    inflate login fragment view only

--- app/src/main/java/com/olamide/findartt/SignInActivity.java
+++ app/src/main/java/com/olamide/findartt/SignInActivity.java
@@ -1,13 +1,30 @@
 package com.olamide.findartt;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class SignInActivity extends AppCompatActivity {
+
+    @BindView(R.id.sign_in_frame)
+    FrameLayout signInFrame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        ButterKnife.bind(this);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        LogInFragment logInFragment = new LogInFragment();
+        fragmentManager.beginTransaction()
+                .add(R.id.sign_in_frame, logInFragment)
+                .commit();
+
     }
 }
